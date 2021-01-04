ffe2fe65c9ed2b12b790f0f6b6d3d41d55af6065
DESKTOP-SMED30I\cocho <elvis.agila@epn.edu.ec>
2021-01-04 13:15:14 -0500

    #14 Auth JWT Proteccion de rutas middleware

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,17 @@ Use App\Article;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
  //   return $request->user();
 //});
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@authenticate');
 Route::get('articles', 'ArticleController@index');
-Route::get('articles/{article}', 'ArticleController@show');
-Route::post('articles', 'ArticleController@store');
-Route::put('articles/{article}', 'ArticleController@update');
-Route::delete('articles/{article}', 'ArticleController@delete');
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+
+    Route::get('user', 'UserController@getAuthenticatedUser');
+    Route::get('articles/{article}', 'ArticleController@show');
+    Route::post('articles', 'ArticleController@store');
+    Route::put('articles/{article}', 'ArticleController@update');
+    Route::delete('articles/{article}', 'ArticleController@delete');
+});
+
+
